68d818fad5534b001b3e81b5b22e5cbec0b195fa
mibbim <michele.berti.g@gmail.com>
2023-02-06 19:27:17 +0100

    MOved from librosa to audio2numpy

--- load_utils.py
+++ load_utils.py
@@ -1,6 +1,7 @@
-import librosa
+from pathlib import Path
+import audio2numpy as a2n
 
 
-def load_mp3(path):
-    signal, sample_rate = librosa.load(path)
+def load_mp3(path: Path):
+    signal, sample_rate = a2n.audio_from_file(path.as_posix())
     return signal, sample_rate
