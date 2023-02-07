56b255ed0572b255105e3ddfa1400ea131069762
mibbim <michele.berti.g@gmail.com>
2023-02-07 12:44:40 +0100

    Implemented recognize and _retrieve best song, testedretrieving elements

--- PandasCorpus.py
+++ PandasCorpus.py
@@ -1,10 +1,13 @@
-from pathlib import Path
-from typing import Dict, Tuple, List
+import os
+import pickle
+from pathlib import Path, PosixPath
+from typing import Dict, Tuple
 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-from Corpus import Corpus, Hash, SongID
+from Corpus import Corpus, Hash, SongID, find_song
 from load_utils import load_mp3
 
 
@@ -30,31 +33,62 @@ class PandasCorpus(Corpus):
         self.corpus = pd.concat(
             (self.corpus, self.dict_to_corpus(hashes)))
 
-    def _get_matches(self, hashes: Dict) -> List[Tuple[int, SongID]]:
+    def _get_matches(self, hashes: Dict[Hash, Tuple[int, SongID]]) -> pd.DataFrame:
         """
 
         :param hashes:
-        :return: List of tuples (time_difference, song_id)
+        :return: Df of tuples (time_difference, song_id)
         """
-        deltas = []
-        for hash_tuple, (t, _) in hashes.items():
-            matches = self.corpus.loc[[hash_tuple]]
-            for _, row in matches.iterrows():
-                deltas.append(((row['time'] - t), row['song_id']))
-        return deltas
+        matches = self.corpus.loc[
+            self.corpus.index.intersection(hashes.keys())]  # Returns a copy, not a view
+        matches.loc[:, "time"] -= [hashes[i][0] for i in matches.index]
+        matches.columns = ['song_id', 'time_diff', ]
+        return matches  # deltas
 
     @staticmethod
     def _get_song_id(song_path: Path):
         return song_path.stem
 
-    # def recognize(self, input_signal: np.ndarray,
-    #               sample_rate: int,
-    #               n_matches_threshold: int = 10):
+    def recognize(self, input_signal: np.ndarray,
+                  sample_rate: int,
+                  n_matches_threshold: int = 10):
+        input_signal = self._preprocess_signal(input_signal)
+        input_hashes = self._get_hashes(input_signal, sample_rate)
+        matching_hashes = self._get_matches(input_hashes)
+        song_id, n_matches, alignment = self._retrieve_best_guess(matching_hashes, )
+        if n_matches < n_matches_threshold:
+            return None, 0, -1
+        matching_confidence = n_matches / len(input_hashes)
+        return song_id, matching_confidence, alignment
+
+    def _retrieve_best_guess(self, matching_hashes: pd.DataFrame, check_neighbour=False):
+        counts = matching_hashes.time_diff.value_counts(sort=True)
+        if check_neighbour:
+            raise NotImplementedError
+        best_alignment_delta, champion_count = counts.index[0], counts.iloc[0]
+        best_songs = matching_hashes.loc[
+            matching_hashes.time_diff == best_alignment_delta, 'song_id'
+        ].value_counts(sort=True)
+        best_song_id, n_matches = best_songs.index[0], best_songs.iloc[0]
+        return best_song_id, n_matches, best_alignment_delta
 
 
 if __name__ == '__main__':
     corpus = PandasCorpus()
     folder_path = Path('data/000')
-    for song_path in tqdm(folder_path.glob('*.mp3')):
-        corpus.add_song(song_path)
-    print(corpus.corpus)
+
+    corpus_file = Path("corpus_pd_0_5.pickle")
+
+    try:
+        with open(corpus_file, "rb") as f:
+            corpus = pickle.load(f)
+    except FileNotFoundError:
+        print("Corpus not found, creating new one...")
+        for track_path in tqdm(os.listdir(folder_path)):
+            corpus.add_song(folder_path / track_path)
+        with open(corpus_file, "wb") as f:
+            pickle.dump(corpus, f)
+
+    for i in range(20):
+        find_song(PosixPath("data/000/000190.mp3"), corpus)
+    print()
